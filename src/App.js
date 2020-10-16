27de43044e5214f0ae1e1011ec0789ff385d8cf6
jeighmz <jls2357@columbia.edu>
2020-10-16 10:29:25 -0400

    added sliders

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSpring, animated, interpolate } from 'react-spring'
+import { useGesture } from 'react-with-gesture'
+import Slider from './slider'
+
 
 function App() {
   return (
-    <div>
-        <h1>you clicked on the link! cool.</h1>
-        
+    <React.Fragment>
+    <div style={{display:'flex', flexDirection:'column', position: "absolute", top: '0px'}}>
+    <div id='current'>
+        <h1 style={{textAlign:'center'}}>you clicked on the link! cool.</h1>
         <div style={{display: 'flex', flexDirection: 'column', alignItems:'center'}}>
-          <img width='150px' height='225px' src={require('./book-covers/something-deeply-hidden.jpg')}></img>
-          <p style={{textAlign:'center'}}>Something Deeply Hidden</p>
-          <p style={{position: 'relative', top: '-20px', textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Sean Carroll</p>
-        </div>
-        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '5px'}}>
-          <img width='150px' height='225px' src={require('./book-covers/book-five-rings.jpg')}></img>
-          <p style={{textAlign:'center'}}>The Complete Book of Five Rings </p>
-          <p style={{position: 'relative', top: '-20px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Miyamoto Musashi edited by Kenji Tokitsu</p>
-        </div>
-        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center',  position:'relative', top: '15px'}}>
-          <img width='150px' height='225px' src={require('./book-covers/cradle-to-cradle.jpg')}></img>
-          <p style={{textAlign:'center'}}>Cradle to Cradle</p>
-          <p style={{position: 'relative', top: '-20px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by William Mcdonough & Michael Braungart </p>
+          <Slider>
+            <img width='150px' height='225px' src={require('./book-covers/something-deeply-hidden.jpg')}></img>
+          </Slider>
+          <div style={{position:'relative', top:'25px'}}>
+            <p style={{textAlign:'center'}}>Something Deeply Hidden</p>
+            <p style={{position: 'relative', top: '-10px', textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Sean Carroll</p>
+          </div>
         </div>
 
+        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '20px'}}>
+          <Slider>
+            <img width='150px' height='225px' src={require('./book-covers/book-five-rings.jpg')}></img>
+          </Slider>
+          <div style={{position:'relative', top:'25px'}}>
+            <p style={{textAlign:'center'}}>The Complete Book of Five Rings </p>
+            <p style={{position: 'relative', top: '-10px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Miyamoto Musashi edited by Kenji Tokitsu</p>
+          </div>
+        </div>
+
+        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center',  position:'relative', top: '40px'}}>
+          <Slider>
+            <img width='150px' height='225px' src={require('./book-covers/cradle-to-cradle.jpg')}></img>
+          </Slider>
+          <div style={{position:'relative', top:'25px'}}>
+            <p style={{textAlign:'center'}}>Cradle to Cradle</p>
+            <p style={{position: 'relative', top: '-10px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by William Mcdonough & Michael Braungart </p>
+          </div>
+        </div>
     </div>
+
+    <div style={{display:'flex', flexDirection:'column', alignItems:'center', position:'relative', top:'60px'}}>
+      <div id='previous'>
+          <h3 style={{textAlign:'center'}}>the knowledge trail</h3>
+      </div>
+      <Slider>
+        <img width='150px' height='225px' src={require('./book-covers/brief_history_time.jpg')}></img>
+      </Slider>
+    </div>
+    </div>
+    </React.Fragment>
   );
 }
 
