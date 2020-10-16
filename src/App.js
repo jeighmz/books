a3c8deb815eb4c69b27ab407b4abcdbd7a632330
jeighmz <jls2357@columbia.edu>
2020-10-16 00:13:26 -0400

    pretty

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ function App() {
     <div>
         <h1>you clicked on the link! cool.</h1>
         
-        <div style={{display: 'flex', flexDirection: 'row', alignItems:'center', justifyContent:'flex-start'}}>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center'}}>
           <img width='150px' height='225px' src={require('./book-covers/something-deeply-hidden.jpg')}></img>
-          <p>Something Deeply Hidden by Sean Carroll</p>
+          <p style={{textAlign:'center'}}>Something Deeply Hidden</p>
+          <p style={{position: 'relative', top: '-20px', textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Sean Carroll</p>
         </div>
-        <div style={{display: 'flex', flexDirection: 'row', alignItems:'center', justifyContent:'flex-start', position:'relative', top: '10px'}}>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '5px'}}>
           <img width='150px' height='225px' src={require('./book-covers/book-five-rings.jpg')}></img>
-          <p>The Complete Book of Five Rings by Miyamoto Musashi edited by Kenji Tokitsu</p>
+          <p style={{textAlign:'center'}}>The Complete Book of Five Rings </p>
+          <p style={{position: 'relative', top: '-20px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Miyamoto Musashi edited by Kenji Tokitsu</p>
         </div>
-        <div style={{display: 'flex', flexDirection: 'row', alignItems:'center', justifyContent:'flex-start',  position:'relative', top: '20px'}}>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center',  position:'relative', top: '15px'}}>
           <img width='150px' height='225px' src={require('./book-covers/cradle-to-cradle.jpg')}></img>
-          <p>Cradle to Cradle by William Mcdonough & Michael Braungart</p>
+          <p style={{textAlign:'center'}}>Cradle to Cradle</p>
+          <p style={{position: 'relative', top: '-20px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by William Mcdonough & Michael Braungart </p>
         </div>
 
     </div>
