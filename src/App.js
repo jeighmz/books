f366a00c95453105a493057d5d3a73e1d69386fc
jeighmz <jls2357@columbia.edu>
2020-10-15 23:25:42 -0400

    added book-covers folder and foundational first book entry

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
   return (
     <div>
         <h1>you clicked on the link! cool.</h1>
-        <p>Something Deeply Hidden by Sean Carroll</p>
+        <div style={{display: 'flex', flexDirection: 'row', alignItems:'center', justifyContent:'center'}}>
+          <img width='150px' height='225px' src={require('./book-covers/something-deeply-hidden.jpg')}></img>
+          <p>Something Deeply Hidden by Sean Carroll</p>
+        </div>
     </div>
   );
 }
