4dde751f342f42e61a0f468a1d9ebe50fee2a0b6
jeighmz <jls2357@columbia.edu>
2020-10-17 01:31:25 -0400

    the knowledge trail added

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ function App() {
           <Slider description={'? what: quantum physics and the possibility of a multiverse.'}>
             <img width='150px' height='225px' src={require('./book-covers/something-deeply-hidden.jpg')}></img>
           </Slider>
-          <div style={{position:'relative', top:'25px'}}>
+          <div style={{position:'relative', top:'15px'}}>
             <p style={{textAlign:'center'}}>Something Deeply Hidden</p>
             <p style={{position: 'relative', top: '-10px', textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Sean Carroll</p>
           </div>
         </div>
 
         <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '20px'}}>
-          <Slider description={'? what: practices of stratgy from 17th century Japanese master swordsman'}>
+          <Slider description={'? what: practices of strategy from 17th century Japanese master swordsman'}>
             <img width='150px' height='225px' src={require('./book-covers/book-five-rings.jpg')}></img>
           </Slider>
-          <div style={{position:'relative', top:'25px'}}>
+          <div style={{position:'relative', top:'15px'}}>
             <p style={{textAlign:'center'}}>The Complete Book of Five Rings </p>
             <p style={{position: 'relative', top: '-10px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by Miyamoto Musashi edited by Kenji Tokitsu</p>
           </div>
@@ -36,7 +36,7 @@ function App() {
           <Slider description={'? what: revolutionary model on eco-friendliness and effectiveness of design'}>
             <img width='150px' height='225px' src={require('./book-covers/cradle-to-cradle.jpg')}></img>
           </Slider>
-          <div style={{position:'relative', top:'25px'}}>
+          <div style={{position:'relative', top:'15px'}}>
             <p style={{textAlign:'center'}}>Cradle to Cradle</p>
             <p style={{position: 'relative', top: '-10px',textAlign:'center', fontSize:'11px', color:'rgba(0,0,0,0.9)'}}> by William Mcdonough & Michael Braungart </p>
           </div>
@@ -47,9 +47,22 @@ function App() {
       <div id='previous'>
           <h3 style={{textAlign:'center'}}>the knowledge trail</h3>
       </div>
-      <Slider description={'? what: everything from time travel to black holes and the place of God in physics'}>
-        <img width='150px' height='225px' src={require('./book-covers/brief_history_time.jpg')}></img>
-      </Slider>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around'}}>
+        <Slider description={'? what: everything from time travel to black holes and the place of God in physics'}>
+          <img width='150px' height='225px' src={require('./book-covers/brief_history_time.jpg')}></img>
+        </Slider>
+        <Slider description={'? what: original but with prettier pictures and smaller sentences'}>
+          <img width='150px' height='225px' src={require('./book-covers/briefer_history_time.jpg')}></img>
+        </Slider>
+      </div>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'20px'}}>
+        <Slider description={'? what: stories about how individuals with power held it throughout history from royalty to con artists'}>
+          <img width='150px' height='225px' src={require('./book-covers/48-laws-of-power.jpg')}></img>
+        </Slider>
+        <Slider description={'? what: short stories about odd paths towards success'}>
+          <img width='150px' height='225px' src={require('./book-covers/barking-up-wrong-tree.jpg')}></img>
+        </Slider>
+      </div>
     </div>
     </div>
     </React.Fragment>
