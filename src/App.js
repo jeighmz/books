2ad7eb7863d2cc793a91ab87c139bee00a118427
James Slater <jamesslater@dyn-160-39-248-138.dyn.columbia.edu>
2021-03-18 23:15:15 -0400

    make over

--- src/App.js
+++ src/App.js
@@ -9,22 +9,15 @@ function App() {
     <React.Fragment>
     <div style={{display:'flex', flexDirection:'column', position: "absolute", top: '0px'}}>
     <div id='current'>
+      <div style={{backgroundColor:'rgba(150, 251, 196,1)', position:"relative", width:'100%', paddingTop:'30px'}}>
         <h2 style={{textAlign:'center'}}>you clicked on the link! cool.</h2>
-        <p style={{textAlign:'center', margin:'25px', fontSize:'12px', color:'rgba(0,0,0,0.7)'}}>peep the books i'm reading. COVID got me picking up books who knew it would be enjoyable.</p>
-        <h2 style={{textAlign:'center' }}>todays binges</h2>
-        <p style={{textAlign:'center', fontSize:'30px', letterSpacing:'8px',color:'rgba(0,125,0,0.9)', position:'relative', top:'-15px'}}>~swipe cover~</p>
+        <p style={{textAlign:'center',position:'relative', top:'-17px',marginLeft:'50px',marginRight:'50px', fontSize:'12px', color:'rgba(0,0,0,0.7)'}}>peep the books i'm reading. COVID got me picking up books who knew it would be enjoyable.</p>
+      </div> 
+        <h2 style={{textAlign:'left', marginLeft:'25px' }}>todays binges</h2>
+        <p style={{textAlign:'center', fontSize:'30px', letterSpacing:'8px',color:'rgba(0,140,0,0.9)', position:'relative', top:'-15px'}}>~swipe cover~</p>
+      
       
         <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '0px'}}>
-          <Slider description={'? what: memoir from former CIA director covering 30 years in government'}>
-            <img width='150px' height='225px' src={require('./book-covers/undaunted.jpg')}></img>
-          </Slider>
-          <div style={{position:'relative', top:'15px'}}>
-            <p style={{textAlign:'center'}}>Undaunted</p>
-            <p style={{position: 'relative', top: '-10px',textAlign:'center', fontSize:'12px', color:'rgba(0,0,0,0.9)'}}> by John O. Brennan</p>
-          </div>
-        </div>
-
-        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '20px'}}>
           <Slider description={'? what: the goal system Youtube, Google, and Intel used to boom'}>
             <img width='150px' height='225px' src={require('./book-covers/Measurewhatmatters.jpg')}></img>
           </Slider>
@@ -36,10 +29,10 @@ function App() {
 
     </div>
 
-    <div style={{display:'flex', flexDirection:'column', alignItems:'center', position:'relative', top:'60px'}}>
+    <div style={{display:'flex', flexDirection:'column', position:'relative', top:'60px'}}>
       <div id='previous'>
-          <h3 style={{textAlign:'center'}}>the knowledge trail</h3>
-          <p style={{textAlign:'center', fontSize:'25px',letterSpacing:'8px', color:'rgba(0,125,0,0.9)', position:'relative', top:'-15px'}}>~tap cover~</p>
+          <h3 style={{textAlign:'left', marginLeft:'25px'}}>the knowledge trail</h3>
+          <p style={{textAlign:'center', fontSize:'25px',letterSpacing:'8px', color:'rgba(0,140,0,0.9)', position:'relative', top:'-15px'}}>~tap cover~</p>
       </div>
       <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'0px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
