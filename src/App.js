3a21e625d994f4616a47abbbe127010d10f61504
jeighmz <48871068+jeighmz@users.noreply.github.com>
2022-01-08 17:16:03 -0500

    added AI 2041

--- src/App.js
+++ src/App.js
@@ -13,10 +13,21 @@ function App() {
         <h2 style={{textAlign:'center'}}>you clicked on the link! cool!</h2>
         <p style={{textAlign:'center',position:'relative', top:'-17px',marginLeft:'50px',marginRight:'50px', fontSize:'12px', color:'rgba(0,0,0,0.7)'}}>peep the books i'm reading.</p>
       </div> 
-        <h2 style={{textAlign:'left', marginLeft:'25px',marginTop:'30px' }}>todays binges</h2>
-        <p style={{textAlign:'center', fontSize:'25px', letterSpacing:'8px',color:'rgba(0,140,0,0.9)', position:'relative', top:'-15px'}}>~swipe cover~</p>
-      
-        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '0px'}}>
+        <h2 style={{textAlign:'left', marginLeft:'25px',marginTop:'30px' }}>Todays Binges</h2>
+        <p style={{textAlign:'center', fontSize:'25px', transform:'scale(1,0.95)', letterSpacing:'8px',color:'rgba(0,140,0,0.9)', position:'relative', top:'-15px'}}>swipe bookcover</p>
+
+        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '-10px'}}>
+          <Slider description={'? what: stories about what to expect from AI in the year 2041'}>
+            <img width='150px' height='225px' src={require('./book-covers/ai2041.jpeg')}></img>
+          </Slider>
+          <div style={{position:'relative', top:'15px'}}>
+            <p style={{textAlign:'center'}}>AI 2041: 10 Visions for Our Future</p>
+            <p style={{position: 'relative', top: '-10px',textAlign:'center', fontSize:'12px', color:'rgba(0,0,0,0.9)'}}> by Kai-Fu Lee</p>
+          </div>
+        </div>
+
+
+        <div style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', position:'relative', top: '10px'}}>
           <Slider description={'? what: methods of problem solving from personal to global scale'}>
             <img width='150px' height='225px' src={require('./book-covers/thinkinginsystems.jpg')}></img>
           </Slider>
@@ -31,17 +42,18 @@ function App() {
 
     <div style={{display:'flex', flexDirection:'column', position:'relative', top:'60px'}}>
       <div id='previous'>
-          <h2 style={{textAlign:'left', marginLeft:'25px'}}>the knowledge trail</h2>
-          <p style={{textAlign:'center', fontSize:'25px',letterSpacing:'8px', color:'rgba(0,140,0,0.9)', position:'relative', top:'-15px'}}>~tap cover~</p>
+          <h2 style={{textAlign:'left', marginLeft:'25px'}}>Previous Books</h2>
+          <p style={{textAlign:'center', fontSize:'25px',letterSpacing:'8px', transform:'scale(1,0.95)', color:'rgba(0,140,0,0.9)', position:'relative', top:'-15px'}}>tap bookcover</p>
       </div>
 
       <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'0px'}}>
+        
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/sagan.jpg')}></img>} rating='? what: Sagans transcripts of his stand at the Gifford Lectures covering his view on God.' takeaways='. my take: more I read ab science n religion the more I find that everybody is equally in the dark searching for answers and hoping to connect their understanding w others.'></Transition>
         </div>
       </div>
 
-      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'0px'}}>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'20px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/dalailama.jpg')}></img>} rating='? what: POV of being spiritual in a largely technological/ science lead society.' takeaways='. my take: science does have a way of making people nihilistic but the Dalai Lama provides alternative ways of analyzing such empirical data through parrallels of science and buddhism.'></Transition>
         </div>
@@ -51,7 +63,7 @@ function App() {
         </div>
       </div>
 
-      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position:'relative', top:'20px'}}>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position:'relative', top:'40px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/dreamland.jpg')}></img>} rating='? what: autobiography from former Area-51 scientist who reverse engineered UFOs' takeaways='. my take: if you dont believe in aliens this will do it. his scienific POV is an engaging twist on a topic most people dismiss.'></Transition>
         </div>
@@ -60,7 +72,7 @@ function App() {
         </div>
       </div>
 
-      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'40px'}}>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'60px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/thefouragreements.jpg')}></img>} rating='? what: thousands of years of toltec knowledge on a virtuous lifestyle' takeaways='. my take: the agreements are decievingly simple to be such precious knowledge - ancient culture needs more credit.'></Transition>
         </div>
@@ -69,7 +81,7 @@ function App() {
         </div>
 
       </div>
-      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'60px'}}>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'80px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/book-five-rings.jpg')}></img>} rating='? what: practices of strategy from 17th century Japanese master swordsman' takeaways='. my take: each scroll: Earth, Water, Fire, Wind and Void make for amazing parallels bewteen nature and crushing the opponent.'></Transition>
         </div>
@@ -77,7 +89,7 @@ function App() {
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/do_polarbears_get_lonely.jpg')}></img>} rating='? what: fifteen years worth of fun facts confirmed by NewScientist magazine' takeaways='. my take: big let down. the cover was intriguing, but all of the facts were either common sense or completely useless. We all know money has OD germs.'></Transition>
         </div>
       </div>
-      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'80px'}}>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'100px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/something-deeply-hidden.jpg')}></img>} rating='? what: theoretical physics behind the possibility of a multiverse' takeaways='. my take: overwhelming amount of particle physics, but besides that Many-Worlds theory is pretty convincing 😅'></Transition>
         </div>
@@ -85,7 +97,7 @@ function App() {
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/cradle-to-cradle.jpg')}></img>} rating='? what: revolutionary model on eco-friendliness and effectiveness of design' takeaways='. my take: why do we always work against nature as if its not the best model for creation - imagine a world with no singing birds... sad'></Transition>
         </div>
       </div>
-      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around',  position: 'relative', top:'100px'}}>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around',  position: 'relative', top:'120px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/brief_history_time.jpg')}></img>} rating='? what: everything from time travel to black holes and the place of God in physics' takeaways='. my take: respect he wrote this while struggling with ALS - where there is life there is hope'></Transition>
         </div>
@@ -93,7 +105,7 @@ function App() {
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/briefer_history_time.jpg')}></img>} rating='? what: original but with prettier pictures and smaller sentences' takeaways='. my take: published couple decades after but still so much unknown in physics '></Transition>
         </div>
       </div>
-      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'120px'}}>
+      <div style={{display:'flex', flexDirection:'row', margin:'0 auto', width:'100%', justifyContent:'space-around', position: 'relative', top:'140px'}}>
         <div style={{position:'relative', top:'0px',width:'150px', height:'225px'}}>
           <Transition content={<img width='150px' height='225px' src={require('./book-covers/barking-up-wrong-tree.jpg')}></img>} rating='? what: short stories about odd paths towards success' takeaways='. my take: who knew you could learn lessons from pirates - inspiration is really everywhere'></Transition>
         </div>
